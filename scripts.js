10c8ebb3e3242259562f1b58240b9cd74817a4f5
vinay3034 <139067756+vinay3034@users.noreply.github.com>
2024-06-15 21:11:02 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.section');
+    
+    const options = {
+        threshold: 0.1
+    };
+
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
